d01a08de3236a0bbecb2bdc37456d0b25efdd912
vijayabharathi <vijayabharathib@users.noreply.github.com>
2017-07-20 07:38:14 +0530

    alter bar chart axis values

--- charts.js
+++ charts.js
@@ -8,8 +8,13 @@ function drawBarChart(data){
   });
   var svg = dimple.newSvg(".container", 1200, 600);
   var myChart = new dimple.chart(svg, flatData);
-  myChart.addTimeAxis("x", "date","%Y-%m-%d","%m-%Y");
-  myChart.addMeasureAxis("y", "gdp");
+  var xAxis=myChart.addTimeAxis("x", "date","%Y-%m-%d","%m-%Y");
+  xAxis.timePeriod=d3.timeYear;
+  xAxis.timeInterval=5;
+  xAxis.title="";
+  xAxis.tickFormat="%Y";
+  var yAxis=myChart.addMeasureAxis("y", "gdp");
+  yAxis.title="Gross Domestic Product, USA";
   myChart.addSeries(null, dimple.plot.bar);
   myChart.draw();
 }
